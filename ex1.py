a7ec95d06b3d63af59d6c64d31139125b8d5d5a2
Moondance-NL <85157648+Moondance-NL@users.noreply.github.com>
2021-07-18 13:39:33 -0500

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,25 @@
+print("I will now count my chickens:")
+# we are calulating the number of hens and roosters
+print("Hens", 25.0 + 30.0 /6.0)
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+# we are calculating the number of eggs
+print(3.0 + 2.0 + 1-5 + 4 % 2-1 / 4.0 + 6.0)
+# we are attemting to find the answer
+print("Is it true that 3 + 2 < 5 - 7?")
+# we are doing a calc to get at the truth
+print(3 + 2 < 5 - 7)
+# doing the math
+print("what is 5 -7?", 5 - 7)
+# and more math
+print("Oh, that's why it's false.")
+#eurika!
+print ("How about some more.")
+#what do you think?
+print("Is it greater?", 5 > -2)
+#getting to the heart of the matter
+print("Is it greater or equal", 5 >= -2)
+#discovering the real real
+print("Is it less or equal?", 5 <= -2)
+#maybe not
